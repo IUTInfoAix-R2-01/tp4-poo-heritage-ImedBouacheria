1129bdfeffc63e78665df6eb2320426d3652d4c4
b24015697 <imed.bouacheria-kebir@etu.univ-amu.fr.com>
2025-02-11 14:15:43 +0100

    teste git

---
+++ src/Cycles/Velo.java
@@ -0,0 +1,19 @@
+package Cycles;
+
+import java.util.Random;
+
+public class Velo {
+	private static final double DEFAUT_BRAQUETTE = 13.0 ;
+	private double braquet;
+	private double diamRoue ;
+	private Random genAlea ;
+	
+	
+		public double getdefaut_braquet () {
+			
+		return braquet ;	
+		}
+		
+		
+		
+}
